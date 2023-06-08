9363a5dc6b7da66a5935e7bcf3a9606a92d5795d
Chris Solorzano <chrisxsolo177@gmail.com>
2023-06-08 16:55:06 -0700

    added modal

--- src/pages/navBar.js
+++ src/pages/navBar.js
@@ -1,13 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Navbar.css';
 
 const Navbar = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   useEffect(() => {
-    // Function to handle scroll event
     const handleScroll = () => {
       const navbar = document.getElementById('navbar');
-
-      // Add 'scrolled' class to navbar when scrolled
       if (window.scrollY > 0) {
         navbar.classList.add('scrolled');
       } else {
@@ -15,25 +14,38 @@ const Navbar = () => {
       }
     };
 
-    // Attach scroll event listener when component mounts
     window.addEventListener('scroll', handleScroll);
 
-    // Clean up the event listener when component unmounts
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
 
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+
+    // Add slide class to the modal element
+    const modal = document.getElementById('nav-modal');
+    modal.classList.add('slide');
+  };
+
   return (
-    <div className="navbar" id="navbar">
+    <nav id="navbar" className="navbar">
       <div className="logo">Chris.dev</div>
-      <ul className="nav-links">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Projects</a></li>
-        <li><a href="#">About Me</a></li>
-        <li><a href="#">Resume</a></li>
-      </ul>
-    </div>
+      <div className={`hamburger ${isModalOpen ? 'open' : ''}`} onClick={toggleModal}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div id="nav-modal" className={`nav-modal ${isModalOpen ? 'open' : ''}`}>
+        <ul className="nav-links">
+          <li><a href="#">Projects</a></li>
+          <li><a href="#">About Me</a></li>
+          <li><a href="#">Resume</a></li>
+          <li><a href="#">Link 4</a></li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
