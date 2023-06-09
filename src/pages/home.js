8e0da862dd2803ab95d1a07064ed2bc14e080ebf
Chris Solorzano <chrisxsolo177@gmail.com>
2023-06-08 17:14:09 -0700

    updated pfp

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,49 @@
-const Home = () => {
-   
+import React, { useEffect, useRef } from 'react';
+import imageSrc from '../components/chris.JPG';
 
-    return (
-      <div className="landing-page">
-      <h1>Software Developer 👋</h1>
-      <p>Hi, I'm Chris Solorzano. Recent Computer Science Graduate based in San Franicsco. 📍</p>
+const Home = () => {
+  const imageRef = useRef(null);
+
+  useEffect(() => {
+    const image = imageRef.current;
+
+    // Add event listener to apply animation on image load
+    image.addEventListener('load', handleImageLoad);
+
+    // Cleanup function to remove event listener
+    return () => {
+      image.removeEventListener('load', handleImageLoad);
+    };
+  }, []);
+
+  const handleImageLoad = () => {
+    const image = imageRef.current;
+
+    // Apply styling and animation to the image
+    image.style.borderRadius = '100%';
+    image.style.width = '65%';
+    image.style.height = '65%';
+    image.style.transition = 'all 0.5s ease';
+  };
+
+  const handleImageClick = () => {
+    const image = imageRef.current;
+
+    // Apply animation on image click
+    image.style.transform = 'rotate(360deg)';
+  };
+
+  return (
+    <div className="landing-page">
+      <div className="text-container">
+        <h1>Software Developer 👋</h1>
+        <p>Hi, I'm Chris Solorzano. Recent Computer Science Graduate based in San Francisco. 📍</p>
+      </div>
+      <div className="image-container">
+        <img ref={imageRef} className="image" src={imageSrc} alt="Image" onClick={handleImageClick} />
+      </div>
     </div>
-    );
-  }
-   
-  export default Home;
+  );
+};
+
+export default Home;
