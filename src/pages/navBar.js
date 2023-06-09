492d021c469c1f2c9fb1f0faaadfb07e1d41e530
Chris Solorzano <chrisxsolo177@gmail.com>
2023-06-08 22:43:22 -0700

    changed hamburger menu styling

--- src/pages/navBar.js
+++ src/pages/navBar.js
@@ -42,7 +42,7 @@ const Navbar = () => {
           <li><a href="#">Projects</a></li>
           <li><a href="#">About Me</a></li>
           <li><a href="#">Resume</a></li>
-          <li><a href="#">Link 4</a></li>
+          <li><a href="#">Contact</a></li>
         </ul>
       </div>
     </nav>
